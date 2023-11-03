9080c6b2af5f8af6309d60ed913cdd7c282938a9
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-11-04 01:50:54 +0530

    route setup for the backend

--- Whatsapp_BE/src/index.js
+++ Whatsapp_BE/src/index.js
@@ -8,8 +8,7 @@ import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 import createHttpError from "http-errors";
-
-
+import routes from "./routes/index.js";
 
 
 dotenv.config();
@@ -18,47 +17,39 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan("dev"));
-
-app.use(helmet());
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(mongoSanitize());
-
-app.use(cookieParser());
-
-app.use(compression());
-
-app.use(fileUpload({
-    useTempFiles: true,
-}));
-
-app.use(cors({
-    origin: 'http://localhost:300',
-}))
+// Middleware
+app.use(morgan("dev")); // Logging middleware
+app.use(helmet()); // Helmet security middleware
+app.use(express.json()); // JSON parsing middleware
+app.use(express.urlencoded({ extended: true })); // URL-encoded data parsing middleware
+app.use(mongoSanitize()); // Sanitize user inputs to prevent NoSQL injection
+app.use(cookieParser()); // Cookie parsing middleware
+app.use(compression()); // Response compression middleware
+app.use(fileUpload({ useTempFiles: true })); // File upload middleware
+app.use(cors({ origin: 'http://localhost:3000' })); // CORS middleware
 
 // Handle GET requests to the root path
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
+app.use("/api/v1", routes);
+
 // Handle POST requests to the root path
 app.post("/", (req, res) => {
     // Handle the POST request logic here
     res.send("POST request received!");
-
+    // You can also send an error response if needed
     // res.status(409).json({ message: "There is a conflict" });
-
 });
 
+// Server startup
 let server = app.listen(PORT, () => {
     console.log("Server is listening at " + PORT);
-    console.log("process:" + process.pid);
-    // throw new error("Error is in app.listen");
+    console.log("Process ID: " + process.pid);
 });
+
+// Exit handlers
 const exitHandler = () => {
     if (server) {
         console.log("********* Server Close *********");
@@ -66,14 +57,18 @@ const exitHandler = () => {
     } else {
         process.exit(1);
     }
-}
-const unExpectedErrorHandler = (err) => {
-    console.log(err);
-    exitHandler();
-}
-process.on("uncaughtException", unExpectedErrorHandler);
-process.on("unhandleRejection", unExpectedErrorHandler);
+};
 
+// Unhandled error handler
+const unExpectedErrorHandler = (err) => {
+    console.error(err);
+    exitHandler();
+};
+
+process.on("uncaughtException", unExpectedErrorHandler);
+process.on("unhandledRejection", unExpectedErrorHandler);
+
+// Error handling middleware
 app.use(async(err, req, res, next) => {
     res.status(err.status || 500);
     res.send({
@@ -81,11 +76,17 @@ app.use(async(err, req, res, next) => {
             status: err.status || 500,
             message: err.message,
         }
-    })
+    });
 });
+
+// Sample route for testing errors
 app.post("/err", (req, res) => {
-    throw createHttpError.BadRequest("this route has an error");
+    throw createHttpError.BadRequest("This route has an error");
 });
+
+// Default route for handling undefined routes
 app.use(async(req, res, next) => {
     next(createHttpError.NotFound("This Route Doesn't Exist"));
-})
+});
+
+// Define your API routes
