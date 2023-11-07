c5fdad3139d3403a0d356e090aa06d88016cb528
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-11-07 12:28:42 +0530

    setting up routing in Frontend

--- root/fe/src/App.js
+++ root/fe/src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
-import { CallIcon} from "./svg";
+import { CallIcon } from "./svg";
+import Home from './pages/home'
 
 function App() {
   return (
     <div className="App">
       <h1>HE;llo</h1>
       <CallIcon className="fill-red-600" />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ root/fe/src/pages/login.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default  function login() {
+  return <div>login</div>;
+}
+
